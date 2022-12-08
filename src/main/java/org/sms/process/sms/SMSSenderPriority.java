27602d63e4cd7ffaea022e8db0755dca3ee94bd0
HuongNV <huongnv@telsoft.com.vn>
2022-12-08 16:23:49 +0700

    - Updated

--- src/main/java/org/sms/process/sms/SMSSenderPriority.java
+++ src/main/java/org/sms/process/sms/SMSSenderPriority.java
@@ -1,8 +1,20 @@
 package org.sms.process.sms;
 
-public class SMSSenderPriority extends SMSSender {
+import java.util.Vector;
+
+public class SMSSenderPriority extends BaseSMSSender {
+    @Override
+    public Vector getParameterDefinition() {
+        Vector vtTemp = super.getParameterDefinition();
+        removeParameterDefinition(vtTemp, "ThreadNumber");
+        removeParameterDefinition(vtTemp, "ThreadCount");
+        return vtTemp;
+    }
+
     @Override
     public void beforeSession() throws Exception {
+        setTotalThread(1);
+        setThreadNumber(0);
         setPriority(" AND NVL(priority,1) = 0 ");
         super.beforeSession();
     }
